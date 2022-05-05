87de9b4c09a5654d84e3f1e9282481c4c5216c65
Michael Kasatkin <m.m.kasatkin@gmail.com>
2022-05-05 10:11:52 +0300

    RequestErrorHabdler class created in /source/utils/request_handler.py and implemented in redmine_client.py [Init_branch]

--- source/core/http_client.py
+++ source/core/http_client.py
@@ -2,6 +2,22 @@ import aiohttp
 
 
 class HTTPClient:
+
+    @staticmethod
+    def request_error_handler(input_request):
+        async def error_wrapped_request(*args, **kwargs):
+            try:
+                response = await input_request(*args, **kwargs)
+                return response
+            except aiohttp.ClientConnectorError as connector_error:
+                return ConnectionError(f"Connection failed: {str(connector_error.host)}")
+            except aiohttp.ClientResponseError as response_error:
+                return ValueError(
+                    f"Resource {str(response_error.message).lower()} "
+                    f"in host {str(response_error.request_info.url.raw_host)}")
+
+        return error_wrapped_request
+
     def __init__(self):
         self.client = aiohttp.ClientSession()
 
--- source/core/redmine_client.py
+++ source/core/redmine_client.py
@@ -1,4 +1,5 @@
 from source.core.http_client import HTTPClient
+from source.utils.request_handler import RequestErrorHandler
 
 
 class RedmineClient:
@@ -8,17 +9,17 @@ class RedmineClient:
         return await new_session.make_request(request_body)
 
     def __init__(self, url: str, token: str):
-        self.http_client: HTTPClient
         self.url = url
         self.token = token
+        self.request_error_handler = RequestErrorHandler()
 
     async def get_redmine_group(self, group_id):
         request_body = self._get_redmine_group_request(group_id=group_id, include="users")
-        return await self.new_session_request(request_body)
+        return await self.request_error_handler.validate(lambda: self.new_session_request(request_body))
 
     async def get_redmine_user_info(self, user_id, start_date, end_date=None):
         request_body = self._get_redmine_user_request(user_id=user_id, from_date=start_date, to_date=end_date)
-        return await self.new_session_request(request_body)
+        return await self.request_error_handler.validate(lambda: request_body)
 
     def _get_redmine_group_request(self, group_id, include=None):
         return "{url}/groups/{group_id}.json?{include}&key={key}".format(
