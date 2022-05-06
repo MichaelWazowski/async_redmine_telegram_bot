7fcc862644033ea989daf11a189a6fd3441aa4da
Mikhail Kasatkin <m.m.kasatkin@gmai.com>
2022-05-06 17:06:27 +0300

    TelegramClient class created in /source/clients/telegram_client.py [Init_branch]

---
+++ source/clients/telegram_client.py
@@ -0,0 +1,9 @@
+import telegram
+
+
+class TelegramClient:
+    def __init__(self, token):
+        self.bot = telegram.Bot(token)
+
+    def send_message(self, chat_id, message):
+        self.bot.send_message(chat_id, message)
