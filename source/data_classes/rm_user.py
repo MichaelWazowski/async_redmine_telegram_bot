bfc187878a0d49e9fcb902afacf3103dd8c25f3a
Mikhail Kasatkin <m.m.kasatkin@gmai.com>
2022-05-06 16:06:11 +0300

    __str__, __format__, _short_name  methods added to RMUser class in /source/data_classes/rm_user.py [Init_branch]

--- source/data_classes/rm_user.py
+++ source/data_classes/rm_user.py
@@ -51,5 +51,21 @@ class RMUser:
         self.hours = hours
         self.comments = comments
 
+    def _short_name(self):
+        surname, first_name, second_name = self._name.split(" ")
+        return f"{surname} {first_name[0]}.{second_name[0]}."
+
     def __repr__(self):
         return f"\n {self._name} (id: {self._id}, hours: {self._hours}, comments: {self._comments})"
+
+    def __str__(self):
+        return format(self)
+
+    def __format__(self, format_spec):
+        component_format_spec = ".1f"
+        prefix, dot, suffix = format_spec.partition(".")
+        if dot:
+            num_decimal_places = int(suffix)
+            component_format_spec = f".{num_decimal_places}f"
+        hours = format(abs(self.hours), component_format_spec)
+        return f"{self._short_name()} часы: {hours}, коментарии: {self._comments} \n"
