dea702ff325698fae156650778478b74b78352b2
Mikhail Kasatkin <m.m.kasatkin@gmai.com>
2022-05-08 11:41:37 +0300

    make_request method of HttpClient class modified to manage single query in /source/clients/http_client.py [Init_branch]

--- source/clients/http_client.py
+++ source/clients/http_client.py
@@ -6,12 +6,8 @@ class HTTPClient:
     def __init__(self):
         self.client = aiohttp.ClientSession()
 
-    async def make_request(self, work_queue):
-        responses_dict = {}
+    async def make_request(self, request):
         async with self.client as session:
-            while not work_queue.empty():
-                query = await work_queue.get()
-                async with session.get(query["url"]) as response:
-                    if not response.raise_for_status():
-                        responses_dict[query["id"]] = await response.json()
-        return responses_dict
+            async with session.get(request) as response:
+                if not response.raise_for_status():
+                    return await response.json()
