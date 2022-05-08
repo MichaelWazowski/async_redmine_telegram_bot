05175b8fbde8528cff1991fd34ee64cb29e9ce79
Mikhail Kasatkin <m.m.kasatkin@gmai.com>
2022-05-08 18:59:07 +0300

    telegram token is being get through os.getenv in /source/workers/sender.py [Init_branch]

--- source/workers/sender.py
+++ source/workers/sender.py
@@ -1,21 +1,21 @@
-import asyncio
 import os
+import asyncio
 from source.clients.telegram_client import TelegramClient
 
 
 class Sender:
     def __init__(self, response_queue: asyncio.Queue, co_senders):
-        self.tg_client = TelegramClient('1959693479:AAEycqFwzSQTgeJhJL9-d9mD4AiE1hKl5pQ')
+        self.tg_client = TelegramClient(os.getenv("TELEGRAM_TOKEN"))
         self.co_senders = co_senders
         self.response_queue = response_queue
 
-    async def sender(self, chat_id):
+    async def sender(self):
         q_item = await self.response_queue.get()
-        self.tg_client.send_message(chat_id, q_item)
+        self.tg_client.send_message(**q_item)
         self.response_queue.task_done()
 
-    async def start(self, chat_id):
-        senders = [asyncio.create_task(self.sender(chat_id)) for number in range(self.co_senders)]
+    async def start(self):
+        senders = [asyncio.create_task(self.sender()) for number in range(self.co_senders)]
         await asyncio.gather(*senders)
         await self.response_queue.join()
         for sender in senders:
