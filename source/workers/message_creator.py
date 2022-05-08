0ee832c7509400473815994d23243a39446c5c06
Mikhail Kasatkin <m.m.kasatkin@gmai.com>
2022-05-08 10:34:49 +0300

    response_queue added in MessageCreator class __init__ method  /source/workers/message_creator.py [Init_branch]

--- source/workers/message_creator.py
+++ source/workers/message_creator.py
@@ -3,19 +3,21 @@ from source.data_classes.rm_group import RMGroup
 
 
 class MessageCreator:
-    def __init__(self, request_queue, coworkers):
+    def __init__(self, request_queue: asyncio.Queue, response_queue: asyncio.Queue, coworkers):
         self.request_queue = request_queue
+        self.response_queue = response_queue
         self.coworkers = coworkers
 
     async def create_message(self):
-        q_item = self.request_queue
+        q_item = await self.request_queue.get()
         if isinstance(q_item, RMGroup):
             message = str(q_item)
             for i, user in enumerate(q_item.users, start=1):
                 message += f"{i}) {user}"
         else:
             message = q_item
-        return message
+        await self.response_queue.put(message)
+        self.request_queue.task_done()
 
     async def start(self):
         workers = [asyncio.create_task(self.create_message()) for number in range(self.coworkers)]
