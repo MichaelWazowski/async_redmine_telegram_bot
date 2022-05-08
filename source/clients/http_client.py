231972bf7f03842502a88337756d89ef64206b66
Mikhail Kasatkin <m.m.kasatkin@gmai.com>
2022-05-08 10:32:15 +0300

    request attribute replaced with work_queue (asyncio.Queue type) /source/clients/http_client.py [Init_branch]

--- source/clients/http_client.py
+++ source/clients/http_client.py
@@ -6,9 +6,12 @@ class HTTPClient:
     def __init__(self):
         self.client = aiohttp.ClientSession()
 
-    async def make_request(self, request):
+    async def make_request(self, work_queue):
+        responses_dict = {}
         async with self.client as session:
-            async with session.get(request) as response:
-                if not response.raise_for_status():
-                    json = await response.json()
-                    return json
+            while not work_queue.empty():
+                query = await work_queue.get()
+                async with session.get(query["url"]) as response:
+                    if not response.raise_for_status():
+                        responses_dict[query["id"]] = await response.json()
+        return responses_dict
