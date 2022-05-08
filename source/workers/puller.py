58a5ce47239ddd39be4d9491ed720756857851c0
Mikhail Kasatkin <m.m.kasatkin@gmai.com>
2022-05-08 10:35:47 +0300

    worker method added to Puller class in   /source/workers/puller.py [Init_branch]

--- source/workers/puller.py
+++ source/workers/puller.py
@@ -5,16 +5,23 @@ from source.data_classes.rm_group import RMGroup
 
 
 class Puller:
-    def __init__(self, url, token):
+    def __init__(self, url, token, pull_queue: asyncio.Queue):
         self.url = url
         self.token = token
-        # self.queue = pull_queue
+        self.queue = pull_queue
         self.rm_client = RedmineClient(url, token)
         self.request_error_handler = RequestErrorHandler()
 
     async def get_redmine_group(self, group_id):
         result = await self.rm_client.get_redmine_group(group_id)
-        if isinstance(result, RMGroup):
-            group = await self.rm_client.get_redmine_group_users_info(result, "2022-05-05")
-            print(group)
         return result
+
+    async def get_user_info(self, group, start_date, end_date):
+        result = await self.rm_client.get_redmine_group_users_info(group, start_date, end_date)
+        return result
+
+    async def worker(self, group_id, start_date, end_date):
+        result = await self.get_redmine_group(group_id)
+        if isinstance(result, RMGroup):
+            result = await self.get_user_info(result, start_date, end_date)
+        await self.queue.put(result)
