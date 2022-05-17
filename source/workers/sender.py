0f966a22883a0b66c80cbf4a74a323c391e74805
Mikhail Kasatkin <m.m.kasatkin@gmai.com>
2022-05-17 11:26:30 +0300

    co_senders attribute has been removed from Sender class in /source/workers/sender.py  [m_k_branch]

--- source/workers/sender.py
+++ source/workers/sender.py
@@ -4,19 +4,15 @@ from source.clients.telegram_client import TelegramClient
 
 
 class Sender:
-    def __init__(self, response_queue: asyncio.Queue, co_senders):
+    def __init__(self, response_queue: asyncio.Queue):
         self.tg_client = TelegramClient(os.getenv("TELEGRAM_TOKEN"))
-        self.co_senders = co_senders
         self.response_queue = response_queue
 
     async def sender(self):
         q_item = await self.response_queue.get()
-        self.tg_client.send_message(**q_item)
+        self.tg_client.send_message(q_item.chat_id, str(q_item))
         self.response_queue.task_done()
 
     async def start(self):
-        senders = [asyncio.create_task(self.sender()) for number in range(self.co_senders)]
-        await asyncio.gather(*senders)
+        await asyncio.gather(asyncio.create_task(self.sender()))
         await self.response_queue.join()
-        for sender in senders:
-            sender.cancel()
