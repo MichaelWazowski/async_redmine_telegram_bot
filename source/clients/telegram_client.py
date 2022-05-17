c7b7cd5790b2373227881110e5f0e6d98fc363bb
Mikhail Kasatkin <m.m.kasatkin@gmai.com>
2022-05-17 11:28:01 +0300

    parse_mode attribute has been added to self.bot.send_message() function implementation in /source/clients/telegram_client.py  [m_k_branch]

--- source/clients/telegram_client.py
+++ source/clients/telegram_client.py
@@ -6,4 +6,4 @@ class TelegramClient:
         self.bot = telegram.Bot(token)
 
     def send_message(self, chat_id, message):
-        self.bot.send_message(chat_id, message)
+        self.bot.send_message(chat_id, message, parse_mode="HTML")
